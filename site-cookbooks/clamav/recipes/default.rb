64cd6a137ff97567a7a58a6b387bb788346cbcb5
Hitoshi.Iwamasa <qzc07405@nifty.com>
2016-06-11 10:48:13 +0900

    I add freshclam.conf in template.

---
+++ site-cookbooks/clamav/recipes/default.rb
@@ -0,0 +1,27 @@
+#
+# Cookbook Name:: clamav
+# Recipe:: default
+#
+# Copyright 2016, Hitoshi Iwamasa
+#
+# All rights reserved - Do Not Redistribute
+#
+# http://www.clamav.net/documents/installing-clamav#rhel
+package "clamav" do
+  action :install
+end
+
+package "clamav-update" do
+  action :install
+end
+
+template "freshclam.conf" do
+  path "/etc/freshclam.conf"
+  owner "root"
+  group "root"
+  mode 0644
+end
+
+#service "clamd" do
+#  action [ :enable, :start ]
+#end
