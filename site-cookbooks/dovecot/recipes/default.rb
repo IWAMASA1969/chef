051d5e417fcf911975fb95a7300bf41e63bfdefd
Hitoshi.Iwamasa <qzc07405@nifty.com>
2016-09-19 22:14:35 +0900

    dovecot
    fixed configuration file for localserver
    telnet 127.0.0.1 143

--- site-cookbooks/dovecot/recipes/default.rb
+++ site-cookbooks/dovecot/recipes/default.rb
@@ -13,3 +13,27 @@ end
 service "dovecot" do
   action [ :enable, :start ]
 end
+
+template "dovecot.conf" do
+  path "/etc/dovecot/dovecot.conf"
+  owner "root"
+  group "root"
+  mode 0644
+  notifies :reload, 'service[dovecot]'
+end
+
+template "10-mail.conf" do
+  path "/etc/dovecot/conf.d/10-mail.conf"
+  owner "root"
+  group "root"
+  mode 0644
+  notifies :reload, 'service[dovecot]'
+end
+
+template "10-auth.conf" do
+  path "/etc/dovecot/conf.d/10-auth.conf"
+  owner "root"
+  group "root"
+  mode 0644
+  notifies :reload, 'service[dovecot]'
+end
