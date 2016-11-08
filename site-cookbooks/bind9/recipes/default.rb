1ee4727aa5086fc1454681d593920336eb2f9150
Hitoshi.Iwamasa <qzc07405@nifty.com>
2016-11-08 23:40:19 +0900

    I add bind9 recipe.
    for studing lpic level2.

---
+++ site-cookbooks/bind9/recipes/default.rb
@@ -0,0 +1,23 @@
+#
+# Cookbook Name:: bind9
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+package "bind" do
+  action :install
+end
+
+package "bind-utils" do
+  action :install
+end
+
+package "bind-chroot" do
+  action :install
+end
+
+service "named" do
+  action [ :enable, :start ]
+end
