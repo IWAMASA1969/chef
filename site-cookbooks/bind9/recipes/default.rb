2dc64bc9e7867303eacc483a33866d2b134aaaea
Hitoshi.Iwamasa <qzc07405@nifty.com>
2016-11-12 00:58:30 +0900

    I add template for 'named.conf'.
    Becouse I can make 'named.conf' to use attribute.

--- site-cookbooks/bind9/recipes/default.rb
+++ site-cookbooks/bind9/recipes/default.rb
@@ -6,18 +6,20 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-package "bind" do
-  action :install
-end
-
-package "bind-utils" do
-  action :install
-end
-
-package "bind-chroot" do
-  action :install
+%w{bind bind-utils bind-chroot}.each do |pkg|
+  package pkg do
+    action :install
+  end
 end
 
 service "named" do
   action [ :enable, :start ]
+end
+
+template "named.conf" do
+  path "/etc/named.conf"
+  owner "named"
+  group "named"
+  mode 0640
+  notifies :reload, 'service[named]'
 end
